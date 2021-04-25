c68fa5bed557618744157dc3a827a7a7145df13f
HuyNguyen <huy8208@gmail.com>
2021-04-24 20:07:42 -0700

    Render total for cart.html

--- project_root/web_app/models.py
+++ project_root/web_app/models.py
@@ -126,18 +126,12 @@ class OrderedItem(models.Model):
     order = models.ForeignKey(Order, on_delete= models.SET_NULL, blank = True, null = True)
     quantity = models.IntegerField(default = 0, null = True, blank = True)
     date_added = models.DateTimeField(auto_now_add=True)
-<<<<<<< HEAD
        
     @property
     def get_total(self):
         """Calculate and return total price for each product in cart."""
         total = self.product.price * self.quantity
         return total
-=======
-
-    
-    
->>>>>>> fdcf2329e4d77daa6bbb72bb160dc504e39977ba
 
 class ShippingAddress(models.Model):
     # Store shipping address of customer

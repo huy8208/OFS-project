63540dae77d8f85b6e4f9ffc94b80e3ea6a4b0f0
HuyNguyen <huy8208@gmail.com>
2021-04-29 01:28:30 -0700

    Add to cart partially done

--- project_root/web_app/views.py
+++ project_root/web_app/views.py
@@ -296,4 +296,24 @@ def product_detail(request,pk):
     return render(request,'Product_detail.html',context={'product':product})
 
 def updateItem(request):
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+    print('Action:',action)
+    print('Product:',productId)
+
+    customer = request.user
+    product = Product.objects.get(id=productId)
+    order,created = Order.objects.get_or_create(customer=customer,complete=False)
+    orderItem,created = OrderedItem.objects.get_or_create(order=order,product=product)
+
+    if action == 'add':
+        orderItem.quantity += 1
+    elif action == 'remove':
+        orderItem.quantity -= 1
+    orderItem.save()
+
+    if orderItem.quantity <= 0:
+        orderItem.delete()
+    
     return JsonResponse('Item was added',safe = False)
